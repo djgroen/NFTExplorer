e1530a5679be62a0cc5312b548df5a5d901f22fe
okean123 <okean.123@mail.ru>
2020-05-14 20:53:12 +0200

    Overhaul and added transfering function

--- NFTExplorer/showNFT.js
+++ NFTExplorer/showNFT.js
@@ -1,6 +1,8 @@
-let ssc = new SSC('https://api.hive-engine.com/rpc');   
-        var JsonGlobal = []  
-        var getData = function(contract, table, account) {
+let ssc = new SSC('https://api.hive-engine.com/rpc');  
+
+var JsonGlobal = [] 
+var getData = function(contract, table, account) {
+                currentTable = table;
                 return new Promise(function(resolve, reject) {
                var JSONdata = [];
                 table = table + "instances";
@@ -16,8 +18,7 @@ let ssc = new SSC('https://api.hive-engine.com/rpc');
                             
                             delete JSONdata[i].population; delete JSONdata[i].education; delete JSONdata[i].creativity; delete JSONdata[i].income; delete JSONdata[i].popularity; delete JSONdata[i].workers;
                             JSONdata[i] = JSON.parse(JSON.stringify( JSONdata[i], ["_id", "name"])); break; 
-                        case 'APIinstances': JSONdata[i] = JSON.parse(JSON.stringify( JSONdata[i].properties, ["x", "info"])); break;
-                        
+                        // case 'APIinstances': JSONdata[i] = JSON.parse(JSON.stringify( JSONdata[i].properties, ["x", "info"])); break;
                            }						
 				}
 			});
@@ -32,11 +33,11 @@ let ssc = new SSC('https://api.hive-engine.com/rpc');
             }
 		
             
-        function buildTable(JSON) {
+function buildTable(dataInJson) {
             // Get data for table header. 
             var col = [];
-            for (var i = 0; i < JSON.length; i++) {
-                for (var key in JSON[i]) {
+            for (var i = 0; i < dataInJson.length; i++) {
+                for (var key in dataInJson[i]) {
                     if (col.indexOf(key) === -1) {
                         col.push(key);
                     }
@@ -65,23 +66,24 @@ let ssc = new SSC('https://api.hive-engine.com/rpc');
             }
             
             // ADD JSON DATA TO THE TABLE AS ROWS.
-            for (var i = 0; i < JSON.length; i++) {
+            for (var i = 0; i < dataInJson.length; i++) {
 
                 tr = table.insertRow(-1);
 
                 for (var j = 0; j < col.length + 1; j++) {
                     if (j < col.length) {
                             var tabCell = tr.insertCell(-1);
-                            tabCell.innerHTML = JSON[i][col[j]];
+                            tabCell.innerHTML = dataInJson[i][col[j]];
                         } 
                     else {
+                        // build the send button
                         var tabCell = tr.insertCell(-1);
                         var btn = document.createElement('button');
                         btn.type = "button";
                         btn.className = "send-btn";
-                        btn.value = JSON[i]._id;
+                        btn.value = JSON.stringify(dataInJson[i]);
                         btn.addEventListener('click', function() {
-                            sendNFT(this.value)
+                            sendNFT(this.value);
                             }, false);
                         btn.innerHTML = '<img src="images/send.png" />';
                         tabCell.appendChild(btn); 
@@ -90,8 +92,7 @@ let ssc = new SSC('https://api.hive-engine.com/rpc');
                 }
             let searchField = document.querySelector('#searchField');
             searchField.style.visibility = "visible";
-            searchField.placeholder="Search for _id..."
-            searchField.placeholder="Search for _id..."
+            searchField.placeholder="Search table..."
             searchField.addEventListener('keyup', filterTable, false);
             }
             
@@ -102,31 +103,67 @@ let ssc = new SSC('https://api.hive-engine.com/rpc');
             
         } // end create table function
         
-        function loadDataButton() {
-            var textHeader = document.getElementById("topText"); 
-			textHeader.innerText = "NFTs for account: @" + document.getElementById("userInput").value
-            
-        }
-        
-        function buildButtonClicked() { 
+function buildButtonClicked() { 
             getJson();
-            setTimeout(() => {  buildTable(JsonGlobal);}, 100); 
+            var textHeader = document.getElementById("topText"); 
+			textHeader.innerText = "NFTs for account: @" + document.getElementById("usernameInput").value
+            setTimeout(() => {  buildTable(JsonGlobal);}, 150); 
             
         }
 
-        async function getJson() {
-            await getData('nft', document.getElementById("game").value, document.getElementById("userInput").value).then( result => JsonGlobal = result);
+async function getJson() {
+            await getData('nft', document.getElementById("game").value, document.getElementById("usernameInput").value).then( result => JsonGlobal = result);
          }
         
-        function clearTable() {
+function clearTable() {
             JSONdata = [];
         }
+        
+        // Opens the pop up
+function sendNFT(buttonData) {
+            buttonData = JSON.parse(buttonData);
+            document.querySelector("#broadcastTXButton").value = buttonData._id;
+            // Opening the pop up
+            var modal = document.querySelector("#myModal");
+            var span = document.querySelector(".close");
+            modal.style.display = "block";
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+            window.onclick = function(event) {
+            if (event.target == modal) {
+                 modal.style.display = "none";
+                }
+            }
+            // end pop up
+        }
 
-        function sendNFT(button) {
-            alert("Send button works! + NFT ID: " + button);
+        // Broadcasts tx (transfer)
+function broadcastSendTX() {
+            var input = document.querySelector("#toAccountInput").value;
+            // check for username errors (wrong ones can still be entered, but this is to prevent mistakes like entering @)
+            if (input.indexOf('@') > -1 || input.length < 3 || input.length > 16 || /\s/.test(input) || input.indexOf('!') > -1 || input.indexOf('"') > -1) {
+                alert('Check the entered username!');
+            }
+            else {
+                // building transaction step by step
+                var transaction = {};
+                transaction.contractName = "nft";
+                transaction.contractAction = "transfer";
+                transaction.contractPayload = {};
+                transaction.contractPayload.to = input;
+                transaction.contractPayload.nfts = [];
+                transaction.contractPayload.nfts[0] ={};
+                transaction.contractPayload.nfts[0].symbol = currentTable;
+                transaction.contractPayload.nfts[0].ids = [];
+                transaction.contractPayload.nfts[0].ids.push(document.querySelector("#broadcastTXButton").value);
+                hive_keychain.requestCustomJson(document.querySelector('#usernameInput'), "ssc-mainnet-hive", "Active", JSON.stringify(transaction), "Send " + currentTable + " NFT with ID " + document.querySelector("#broadcastTXButton").value + " to: " + input, function(response) {
+	               console.log(response);
+                });
+            }
         }
         
-         function filterTable(event) {
+function filterTable(event) {
             var filter = event.target.value.toUpperCase();
             var table = document.querySelector("#jsonDataTable")
             var rows = document.querySelector("#jsonDataTable tbody").rows;
@@ -182,28 +219,28 @@ let ssc = new SSC('https://api.hive-engine.com/rpc');
                             rows[i].style.display = "none";
                             } 
                          break;
-                     case 7:
+                      case 7:
                          if (countCol[0].indexOf(filter) > -1 || countCol[1].indexOf(filter) > -1 || countCol[2].indexOf(filter) > -1 || countCol[3].indexOf(filter) > -1 || countCol[4].indexOf(filter) > -1 || countCol[5].indexOf(filter) > -1 || countCol[6].indexOf(filter) > -1) {
                             rows[i].style.display = "";
                          } else {
                             rows[i].style.display = "none";
                             } 
                          break;
-                     case 8:
+                      case 8:
                          if (countCol[0].indexOf(filter) > -1 || countCol[1].indexOf(filter) > -1 || countCol[2].indexOf(filter) > -1 || countCol[3].indexOf(filter) > -1 || countCol[4].indexOf(filter) > -1 || countCol[5].indexOf(filter) > -1 || countCol[6].indexOf(filter) > -1 || countCol[7].indexOf(filter) > -1) {
                             rows[i].style.display = "";
                          } else {
                             rows[i].style.display = "none";
                             } 
                          break;
-                     case 9:
+                      case 9:
                          if (countCol[0].indexOf(filter) > -1 || countCol[1].indexOf(filter) > -1 || countCol[2].indexOf(filter) > -1 || countCol[3].indexOf(filter) > -1 || countCol[4].indexOf(filter) > -1 || countCol[5].indexOf(filter) > -1 || countCol[6].indexOf(filter) > -1 || countCol[7].indexOf(filter) > -1 || countCol[8].indexOf(filter) > -1) {
                             rows[i].style.display = "";
                          } else {
                             rows[i].style.display = "none";
                             } 
                          break;
-                     case 10:
+                      case 10:
                          if (countCol[0].indexOf(filter) > -1 || countCol[1].indexOf(filter) > -1 || countCol[2].indexOf(filter) > -1 || countCol[3].indexOf(filter) > -1 || countCol[4].indexOf(filter) > -1 || countCol[5].indexOf(filter) > -1 || countCol[6].indexOf(filter) > -1 || countCol[7].indexOf(filter) > -1 || countCol[8].indexOf(filter) > -1 || countCol[9].indexOf(filter) > -1) {
                             rows[i].style.display = "";
                          } else {
@@ -211,6 +248,6 @@ let ssc = new SSC('https://api.hive-engine.com/rpc');
                             } 
                          break;
                         }
-            }   
-        }   
+                        }
+            } 
   
