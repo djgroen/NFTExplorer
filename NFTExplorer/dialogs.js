b48c2f1aa176235fb0d26483370a896512f85c7c
okean123 <okean.123@mail.ru>
2020-07-18 19:09:46 +0200

    Added sell multiple NFTs function // restructured files

--- NFTExplorer/dialogs.js
+++ NFTExplorer/dialogs.js
@@ -20,8 +20,10 @@ function dialogClickHandler(e) {
 
 function getDialog(type,data) {
     $("#modalDialog").html("");
+    $("#modalDialog").append("<dialog id ='modalDialogContent'></dialog>");
     switch(type) {
-        case 'singleSell': buildSingleSell(data); break;       
+        case 'singleSell': buildSingleSell(data); break;    
+        case 'multiSell': buildMultiSell(data); break;
            }
     window.onclick = function(event) {   
         dialogClickHandler(event);
@@ -30,18 +32,34 @@ function getDialog(type,data) {
 }
 
 function buildSingleSell(data) {
-    $("#modalDialog").append("<dialog id ='modalDialogContent'></dialog>"); 
-            $("#modalDialogContent").append("<label for = 'priceInput' > Price:</label>");
-            $("#modalDialogContent").append("<input class = 'inputField' id = 'priceInput' type='number' placeholder='Price' name='priceInput' required>");
-            $("#modalDialogContent").append("<label for = 'priceSymbolInput' > Symbol:</label>");
-            $("#modalDialogContent").append("<input class = 'inputField' id = 'priceSymbolInput' type='text' placeholder='Price Symbol' name='priceSymbolInput' required>");
-            $("#modalDialogContent").append("<br>");
-            $("#modalDialogContent").append("<br>");
-            $("#modalDialogContent").append('<button class = "mainButton" id = "dialogButton"> Sell</button>');
-            $( "#dialogButton" ).click(function() {
-                sellNFT(data, $('#priceInput').val(), $('#priceSymbolInput').val());          
-            });
-            $("#priceSymbolInput").autocomplete({
-                source: symbolSuggestions
-            });
+    $("#modalDialogContent").append("<label for = 'priceInput' > Price:</label>");
+    $("#modalDialogContent").append("<input class = 'inputField' id = 'priceInput' type='number' placeholder='Price' name='priceInput' required>");
+    $("#modalDialogContent").append("<label for = 'priceSymbolInput' > Symbol:</label>");
+    $("#modalDialogContent").append("<input class = 'inputField' id = 'priceSymbolInput' type='text' placeholder='Price Symbol' name='priceSymbolInput' required>");
+    $("#modalDialogContent").append("<br>");
+    $("#modalDialogContent").append("<br>");
+    $("#modalDialogContent").append('<button class = "mainButton" id = "dialogButton"> Sell</button>');
+    $( "#dialogButton" ).click(function() {
+            sellNFT(data, $('#priceInput').val(), $('#priceSymbolInput').val());          
+                });
+    $("#priceSymbolInput").autocomplete({
+        source: symbolSuggestions
+    });
+}
+
+function buildMultiSell(data) {
+    $("#modalDialogContent").append("<label for = 'priceInput' > Price:</label>");
+    $("#modalDialogContent").append("<input class = 'inputField' id = 'priceInput' type='number' placeholder='Price' name='priceInput' required>");
+    $("#modalDialogContent").append("<label for = 'priceSymbolInput' > Symbol:</label>");
+    $("#modalDialogContent").append("<input class = 'inputField' id = 'priceSymbolInput' type='text' placeholder='Price Symbol' name='priceSymbolInput' required>");
+    $("#modalDialogContent").append("<br>");
+    $("#modalDialogContent").append("<br>");
+    $("#modalDialogContent").append('<button class = "mainButton" id = "dialogButton"> Sell</button>');
+    $( "#dialogButton" ).click(function() {
+            sellmultipleNFTs(data, $('#priceInput').val(), $('#priceSymbolInput').val());          
+                });
+    $("#priceSymbolInput").autocomplete({
+        source: symbolSuggestions
+    });
+    $("#modalDialogContent").append("<p> Note: This will sell all selected NFTs for the same price </p>");
 }
